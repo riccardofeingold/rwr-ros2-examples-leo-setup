db6f9e5cc0a37bab2bb3975a4ab6154da3cbf8b7
Riccardo Feingold <riccardofeingold@gmail.com>
2025-04-04 16:53:07 +0200

    WIP

--- src/joy_to_twist.cpp
+++ src/joy_to_twist.cpp
@@ -7,10 +7,11 @@
 #include <iostream>
 #include <mutex>
 #include <thread>
+#include <array>
 
 // ROS2 includes.
 #include "rclcpp/rclcpp.hpp"
-#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 
 class JoyToTwistNode : public rclcpp::Node {
@@ -18,13 +19,17 @@ class JoyToTwistNode : public rclcpp::Node {
   JoyToTwistNode()
       : Node("joy_to_twist"), debounce_counter_(0), enabled_(false) {
     // Initialize parameters
-    twist_topic_ = this->declare_parameter<std::string>("twist_topic", "/franka/end_effector_twist_cmd");
+    pos_cmd_topic_ = this->declare_parameter<std::string>("twist_topic", "/franka/end_effector_pose_cmd");
+    pose_topic_ = this->declare_parameter<std::string>("pose_topic", "/franka/end_effector_pose");
     joy_topic_ = this->declare_parameter<std::string>("joy_topic", "/joy");
 
     // Create publishers
-    twist_pub_ = this->create_publisher<geometry_msgs::msg::Twist>(twist_topic_, 10);
+    pos_cmd_pub_ = this->create_publisher<geometry_msgs::msg::PoseStamped>(pos_cmd_topic_, 10);
+
 
     // Create subscriber
+    pose_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
+      pose_topic_, 10, std::bind(&JoyToTwistNode::poseCallback, this, std::placeholders::_1));
     joy_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
         joy_topic_, 10, std::bind(&JoyToTwistNode::joyCallback, this, std::placeholders::_1));
 
@@ -33,12 +38,34 @@ class JoyToTwistNode : public rclcpp::Node {
 
  private:
   // Member variables for publishers, subscriber, and parameters.
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub_;
+  rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pos_cmd_pub_;
   rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
+  rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub_;
 
-  std::string twist_topic_, robot_cmd_topic_, gripper_cmd_topic_, joy_topic_;
+  std::string pos_cmd_topic_, robot_cmd_topic_, gripper_cmd_topic_, joy_topic_, pose_topic_;
   uint16_t debounce_counter_;
   bool enabled_;
+  std::array<double, 3> position = {0, 0, 0};
+  std::array<double, 4> orientation = {1, 0, 0, 0};
+  bool initialized_ = false;
+
+  void poseCallback(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
+  {
+    if (initialized_)
+    {
+      return;
+    }
+    position[0] = msg->pose.position.x;
+    position[1] = msg->pose.position.y;
+    position[2] = msg->pose.position.z;
+
+    orientation[0] = msg->pose.orientation.w;
+    orientation[1] = msg->pose.orientation.x;
+    orientation[2] = msg->pose.orientation.y;
+    orientation[3] = msg->pose.orientation.z;
+    RCLCPP_INFO(this->get_logger(), "INIT-------------------------------------");
+    initialized_ = true;
+  }
 
   void joyCallback(const sensor_msgs::msg::Joy::SharedPtr msg) {
     if (msg->axes.size() != 6) {
@@ -81,18 +108,30 @@ class JoyToTwistNode : public rclcpp::Node {
       RCLCPP_INFO(this->get_logger(), "{%s}: Joystick to Twist", (enabled_ ? "ENABLED" : "DISABLED"));
     }
 
-    // Publish twist commands if enabled
-    if (enabled_) {
-      geometry_msgs::msg::Twist twist_msg;
-      twist_msg.linear.x = scaling * analogRight_y;
-      twist_msg.linear.y = scaling * analogRight_x;
-      twist_msg.linear.z = - scaling * dPad_y;
-      twist_msg.angular.x = -scaling * analogLeft_x;
-      twist_msg.angular.y = scaling * analogLeft_y;
-      twist_msg.angular.z = scaling * dPad_x;
-      twist_pub_->publish(twist_msg);
+    if (buttonY)
+    {
+      position[2] += 0.01;
+      RCLCPP_INFO(this->get_logger(), "Received Joy input");
     }
 
+    if (buttonA)
+    {
+      position[2] -= 0.01;
+    }
+    // Publish twist commands if enabled
+    if (initialized_) {
+      geometry_msgs::msg::PoseStamped pose_msg;
+      pose_msg.pose.position.x = position[0];
+      pose_msg.pose.position.y = position[1];
+      pose_msg.pose.position.z = position[2];
+
+      pose_msg.pose.orientation.w = orientation[0];
+      pose_msg.pose.orientation.x = orientation[1];
+      pose_msg.pose.orientation.y = orientation[2];
+      pose_msg.pose.orientation.z = orientation[3];
+
+      pos_cmd_pub_->publish(pose_msg);
+    }
   }
 };
 
